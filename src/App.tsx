f290d55e6eade75a32aa750d3ce69662c61bd88d
sovilleda07 <sovilleda07@gmail.com>
2026-01-12 17:01:35 -0600

    Update Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { BackgroundBlobs } from './layout/BackgroundBlobs';
 import { Header } from './layout/Header';
-import { Footer } from './layout/Footer';
 
 function App() {
   return (
-    <div className="relative min-h-screen w-full">
+    <div className="relative flex min-h-screen w-full flex-col">
       <BackgroundBlobs />
-
-      <div className="relative flex min-h-screen flex-col">
-        <Header />
-        <main className="flex-1"></main>
-        <Footer />
-      </div>
+      <Header />
     </div>
   );
 }
