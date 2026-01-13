f918142571cdf5c37b3868b70a95a79380ace578
sovilleda07 <sovilleda07@gmail.com>
2026-01-12 22:11:31 -0600

    Create Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { BackgroundBlobs } from './components/background/BackgroundBlobs';
 import { Header } from './components/layout/Header';
+import { Hero } from './components/sections/Hero';
 
 function App() {
   return (
     <div className="relative flex min-h-screen w-full flex-col">
       <BackgroundBlobs />
       <Header />
+
+      <main className="flex flex-col items-center w-full px-4 md:px-10">
+        <Hero />
+      </main>
     </div>
   );
 }
