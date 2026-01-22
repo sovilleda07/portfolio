c344c67246df0556a8c0445f66fd410038e4ef0c
sovilleda07 <sovilleda07@gmail.com>
2026-01-21 21:09:44 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { About } from './components/sections/About';
 import { Skills } from './components/sections/Skills/Skills';
 import { Projects } from './components/sections/Projects/Projects';
 import { Contact } from './components/sections/Contact/Contact';
-import { Footer } from './components/layout/Footer';
+import { Footer } from './components/layout/Footer/Footer';
 
 function App() {
   return (
