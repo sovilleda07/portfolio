af5e76d57dd355763c3ec6a1ef18646eb9182590
sovilleda07 <sovilleda07@gmail.com>
2026-01-21 19:14:27 -0600

    Update Contact component and create ContactItem component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Hero } from './components/sections/Hero';
 import { About } from './components/sections/About';
 import { Skills } from './components/sections/Skills/Skills';
 import { Projects } from './components/sections/Projects/Projects';
-import { Contact } from './components/sections/Contact';
+import { Contact } from './components/sections/Contact/Contact';
 import { Footer } from './components/layout/Footer';
 
 function App() {
